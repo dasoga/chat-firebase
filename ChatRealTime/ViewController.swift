db23638ec44d7f7fc456aba15ca75be2b7e9fe0d
Dante Solorio <contact@deipi.com>
2016-07-05 10:47:20 -0500

    Initial Commit

---
+++ ChatRealTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatRealTime
+//
+//  Created by Dante Solorio on 7/5/16.
+//  Copyright © 2016 Dante Solorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
