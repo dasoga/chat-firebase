ebb5a03e0801b78edbcdc75ac98f96ba304bd297
Dante Solorio <contact@deipi.com>
2016-07-08 13:34:45 -0500

    Register user with It profile image

---
+++ ChatRealTime/Controller/LoginController+handlers.swift
@@ -0,0 +1,144 @@
+//
+//  LoginController+handlers.swift
+//  ChatRealTime
+//
+//  Created by Dante Solorio on 7/8/16.
+//  Copyright © 2016 Dante Solorio. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+extension LoginViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func handleLoginRegister(){
+        if loginRegisterSegmentedControl.selectedSegmentIndex == 0{
+            handleLogin()
+        }else{
+            handleRegister()
+        }
+    }
+    
+    func handleLogin(){
+        guard let email = emailTextField.text, password = passwordTextField.text else{
+            print("Form is not valid")
+            return
+        }
+        
+        FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (user, error) in
+            if error != nil{
+                print(error)
+                return
+            }
+            
+            // Successfully logged in our user
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })
+    }
+    
+    func handleRegister(){
+        guard let email = emailTextField.text, password = passwordTextField.text, name = nameTextField.text else{
+            print("Form is not valid")
+            return
+        }
+        
+        FIRAuth.auth()?.createUserWithEmail(email, password: password, completion: { (user: FIRUser?, error) in
+            if error != nil{
+                print(error)
+                return
+            }
+            
+            
+            guard let uid = user?.uid else{
+                return
+            }
+            
+            // Successfully
+            let imageName = NSUUID().UUIDString
+            let storageRef = FIRStorage.storage().reference().child("profile_imges").child("\(imageName).png")
+            
+            if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!){
+                storageRef.putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                    if error != nil{
+                        print(error)
+                        return
+                    }
+                    
+                    if let profileImageUrl = metadata?.downloadURL()?.absoluteString{
+                        let values = ["name": name, "email": email, "profileImageUrl": profileImageUrl]
+                        self.registerUserIntoDatabaseWithUID(uid, values: values)
+                    }
+                })
+            }
+
+            
+            
+            
+        })
+    }
+    
+    private func registerUserIntoDatabaseWithUID(uid:String, values: [String:AnyObject]){
+        let ref = FIRDatabase.database().referenceFromURL("https://chatrealtime-1e87d.firebaseio.com/")
+        let usersReference = ref.child("users").child(uid)
+        usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            if err != nil{
+                print(err)
+                return
+            }
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })
+    }
+    
+    func handleRegisterChange(){
+        let title = loginRegisterSegmentedControl.titleForSegmentAtIndex(loginRegisterSegmentedControl.selectedSegmentIndex)
+        loginRegisterButton.setTitle(title, forState: .Normal)
+        
+        // Change height of inputContainerView
+        inputContainerViewHeightAnchor?.constant = loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 100 : 150
+        
+        // Change height of name text field
+        nameTextFieldHeightAnchor?.active = false
+        nameTextFieldHeightAnchor = nameTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 0 : 1/3)
+        nameTextFieldHeightAnchor?.active = true
+        
+        // Change height of email text field
+        emailTextFieldHeightAnchor?.active = false
+        emailTextFieldHeightAnchor = emailTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
+        emailTextFieldHeightAnchor?.active = true
+        
+        // Change height of password text field
+        passwordTextFieldHeightAnchor?.active = false
+        passwordTextFieldHeightAnchor = passwordTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
+        passwordTextFieldHeightAnchor?.active = true
+    }
+    
+    func handleSelectProfileImageView(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("Canceled picker")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage{
+           selectedImageFromPicker = editedImage
+        }else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker{
+            profileImageView.image = selectedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- ChatRealTime/Controller/LoginViewController.swift
+++ ChatRealTime/Controller/LoginViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 
 class LoginViewController: UIViewController {
     
@@ -69,11 +68,13 @@ class LoginViewController: UIViewController {
         return tf
     }()
     
-    let profileImageView: UIImageView = {
+    lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "profileImage")
         imageView.contentMode = .ScaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
+        imageView.userInteractionEnabled = true
         return imageView
     }()
     
@@ -178,89 +179,7 @@ class LoginViewController: UIViewController {
         loginRegisterSegmentedControl.widthAnchor.constraintEqualToAnchor(inputsContainerView.widthAnchor).active = true
         loginRegisterSegmentedControl.heightAnchor.constraintEqualToConstant(36).active = true
     }
-    
-    func handleLoginRegister(){
-        if loginRegisterSegmentedControl.selectedSegmentIndex == 0{
-            handleLogin()
-        }else{
-            handleRegister()
-        }
-    }
-    
-    func handleLogin(){
-        guard let email = emailTextField.text, password = passwordTextField.text else{
-            print("Form is not valid")
-            return
-        }
-        
-        FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (user, error) in
-            if error != nil{
-                print(error)
-                return
-            }
-            
-            // Successfully logged in our user
-            self.dismissViewControllerAnimated(true, completion: nil)
-        })
-    }
-    
-    func handleRegister(){
-        guard let email = emailTextField.text, password = passwordTextField.text, name = nameTextField.text else{
-            print("Form is not valid")
-            return
-        }
-        
-        FIRAuth.auth()?.createUserWithEmail(email, password: password, completion: { (user: FIRUser?, error) in
-            if error != nil{
-                print(error)
-                return
-            }
-            
-            
-            guard let uid = user?.uid else{
-                return
-            }
-            
-            // Successfully
-            let ref = FIRDatabase.database().referenceFromURL("https://chatrealtime-1e87d.firebaseio.com/")
-            let usersReference = ref.child("users").child(uid)
-            let values = ["name": name, "email": email]
-            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-                if err != nil{
-                    print(err)
-                    return
-                }
-                
-                self.dismissViewControllerAnimated(true, completion: nil)
-            })
 
-            
-            
-        })
-    }
-    
-    func handleRegisterChange(){
-        let title = loginRegisterSegmentedControl.titleForSegmentAtIndex(loginRegisterSegmentedControl.selectedSegmentIndex)
-        loginRegisterButton.setTitle(title, forState: .Normal)
-        
-        // Change height of inputContainerView
-        inputContainerViewHeightAnchor?.constant = loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 100 : 150
-        
-        // Change height of name text field
-        nameTextFieldHeightAnchor?.active = false
-        nameTextFieldHeightAnchor = nameTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 0 : 1/3)
-        nameTextFieldHeightAnchor?.active = true
-        
-        // Change height of email text field
-        emailTextFieldHeightAnchor?.active = false
-        emailTextFieldHeightAnchor = emailTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-        emailTextFieldHeightAnchor?.active = true
-        
-        // Change height of password text field
-        passwordTextFieldHeightAnchor?.active = false
-        passwordTextFieldHeightAnchor = passwordTextField.heightAnchor.constraintEqualToAnchor(inputsContainerView.heightAnchor, multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-        passwordTextFieldHeightAnchor?.active = true
-    }
 }
 
 
